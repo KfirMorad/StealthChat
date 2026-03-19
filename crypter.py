3cd4574ffa10fdee8e970bc1141bb72ec0c1d9e6
Kfir Morad <kfir212160@gmail.com>
2026-03-20 01:10:07 +0200

    da

--- crypter.py
+++ crypter.py
@@ -7,8 +7,7 @@ from cryptography.fernet import Fernet
 session_passwords: dict[str, str] = {}
 
 
-def _derive_key(pwd: str, salt: bytes) -> bytes:
-    return hashlib.pbkdf2_hmac("sha256", pwd.encode(), salt, 100_000)
+
 
 
 def encrypt_message(plain: str, pwd: str) -> bytes:
@@ -16,6 +15,8 @@ def encrypt_message(plain: str, pwd: str) -> bytes:
     key = base64.urlsafe_b64encode(_derive_key(pwd, salt))
     return salt + Fernet(key).encrypt(plain.encode())
 
+def _derive_key(pwd: str, salt: bytes) -> bytes:
+    return hashlib.pbkdf2_hmac("sha256", pwd.encode(), salt, 100_000)
 
 def decrypt_message(cipher: bytes, pwd: str) -> str:
     salt, token = cipher[:16], cipher[16:]
